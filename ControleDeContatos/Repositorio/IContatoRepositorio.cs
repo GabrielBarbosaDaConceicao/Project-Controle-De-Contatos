d817cacf80dca78654f9d0ebbd6ae91bf74bff8f
Gabriel Barbosa Da Conceição <gabriel.bc.dev@hotmail.com>
2022-01-27 16:47:46 -0300

    Add Project

---
+++ ControleDeContatos/Repositorio/IContatoRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using ControleDeContatos.Models;
+using System.Collections.Generic;
+
+
+namespace ControleDeContatos.Repositorio
+{
+    public interface IContatoRepositorio
+    {
+        ContatoModel ListarPorId(int id);
+        List<ContatoModel> BuscarTodos();
+        ContatoModel Adicionar(ContatoModel contado);
+        ContatoModel Atualizar(ContatoModel contato);
+        bool Apagar(int id);
+
+    }
+}
